5191d204111c91fd6d10cfbe959833bc4017f65e
David Darling <ddarling@uams.edu>
2016-06-06 17:45:42 -0500

    Added Apache 2.0 License to source code.

--- PyGeoCoderRev/GeoCoderRev.py
+++ PyGeoCoderRev/GeoCoderRev.py
@@ -1,3 +1,21 @@
+# ========================================================================
+#
+# Copyright © 2016 Khepry Quixote
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ========================================================================
+
 import argparse
 import csv
 import io
@@ -28,7 +46,7 @@ def quotemode_xlator(quote_mode_str):
     
     return quote_mode_val
 
-arg_parser = argparse.ArgumentParser(prog='%s' % pgm_name, description='Reverse geo-code an NCEDC-formatted earthquake file.')
+arg_parser = argparse.ArgumentParser(prog='%s' % pgm_name, description='Reverse geo-code an NCEDC-formatted earthquake CSV file.')
 
 arg_parser.add_argument('--src-file-path', required=True, help='source file path')
 arg_parser.add_argument('--src-delimiter', default=',', help='source file delimiter character')
